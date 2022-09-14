e4f541272c487eb321c3483a32d4fc1c56e9d547
pfrost982 <pfrost982@gmail.com>
2022-09-14 21:18:09 +0700

    countdown timer

--- app/src/main/java/ru/gb/himike/ui/home/HomeFragment.kt
+++ app/src/main/java/ru/gb/himike/ui/home/HomeFragment.kt
@@ -12,9 +12,6 @@ import ru.gb.himike.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -23,14 +20,13 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        homeViewModel.countdownString.observe(viewLifecycleOwner) {
+            binding.timerData.count.text = it
         }
         return root
     }
--- app/src/main/java/ru/gb/himike/ui/home/HomeViewModel.kt
+++ app/src/main/java/ru/gb/himike/ui/home/HomeViewModel.kt
@@ -1,13 +1,45 @@
 package ru.gb.himike.ui.home
 
-import androidx.lifecycle.LiveData
+import android.os.CountDownTimer
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.*
 
 class HomeViewModel : ViewModel() {
+    private val _countdownString = MutableLiveData<String>()
+    val countdownString = _countdownString
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    init {
+        countdownStart()
     }
-    val text: LiveData<String> = _text
+
+    private fun countdownStart() {
+
+        val endDate = Calendar.getInstance()
+        endDate.add(Calendar.DATE, 5)
+
+        object : CountDownTimer(endDate.time.time - Calendar.getInstance().time.time, 2000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                var millisLeft = millisUntilFinished
+                val secondsInMilli = 1000L
+                val minutesInMilli = secondsInMilli * 60
+                val hoursInMilli = minutesInMilli * 60
+                val daysInMilli = hoursInMilli * 24
+
+                val daysLeft = millisUntilFinished / daysInMilli
+                millisLeft %= daysInMilli
+                val hoursLeft = millisLeft / hoursInMilli
+                millisLeft %= hoursInMilli
+                val minutesLeft = millisLeft / minutesInMilli
+                millisLeft %= minutesInMilli
+                //${String.format("%.2d", hoursLeft)}
+                _countdownString.postValue("${daysLeft}:${hoursLeft}:${minutesLeft}")
+            }
+
+            override fun onFinish() {
+                _countdownString.postValue("Time Is Over!!!")
+            }
+        }.start()
+    }
 }
